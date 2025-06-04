1b67155e020135e062a82e37b1d9a33b3240b27f
ABHISHEK <ay22oct02@gmail.com>
2025-06-04 22:30:38 +0530

    code restructring

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,5 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+mod shell;
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let stdin = io::stdin();
-    loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
-        
-        let input = input.trim();
-        match input {
-            "exit" => break,
-            "exit 0" => break,
-            command=>{
-                let commandInput: Vec<&str> = command.splitn(2, ' ').collect();
-
-                // print!(echo[0]);
-                // print!("{}", echo[0]);
-                // println!("{}", echo[1]);
-                let a = Vec::new();
-                let cmd = a.get(3);
-                match cmd {
-                    Some(&"echo")=>{
-                        if commandInput.len() > 1{
-                            println!("{}", commandInput[1]);
-                        }
-                    }
-                    Some(&"type")=>{
-                        if commandInput.len() > 1 {
-                            println!("{}", commandInput[1]);
-                        }
-                    }
-                    Some(cmd) => {
-                        println!("{}: command not found", cmd);
-                    },
-                    None=>println!("Enter a command"),
-                    // _ => {}
-                }
-                // if echo.len() > 1 && echo[0] == "echo" {
-                //     println!("{}", echo[1]);
-                // }
-                // else {
-                    println!("{}: command not found", command)
-                // }
-            },
-        }
-    }
+    shell::run();
 }
---
+++ src/shell.rs
@@ -0,0 +1,58 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
+pub fn run() {
+    // Main shell loop here
+    // Uncomment this block to pass the first stage
+    // print!("$ ");
+    // io::stdout().flush().unwrap();
+
+    // Wait for user input
+    let stdin = io::stdin();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        
+        let input = input.trim();
+        match input {
+            "exit" => break,
+            "exit 0" => break,
+            command=>{
+                let commandInput: Vec<&str> = command.splitn(2, ' ').collect();
+
+                // print!(echo[0]);
+                // print!("{}", echo[0]);
+                // println!("{}", echo[1]);
+                // let a = Vec::new();
+                let cmd = commandInput.get(0);
+                // println!("{}", cmd.unwrap());
+                match cmd {
+                    Some(&"echo")=>{
+                        print!("hhh");
+                        if commandInput.len() > 1{
+                            println!("{}", commandInput[1]);
+                        }
+                    },
+                    Some(&"type")=>{
+                        if commandInput.len() > 1 {
+                            println!("{}", commandInput[1]);
+                        }
+                    },
+                    Some(cmd) => {
+                        println!("{}: command not found", cmd);
+                    },
+                    None=>println!("Enter a command"),
+                    // _ => {}
+                }
+                // if echo.len() > 1 && echo[0] == "echo" {
+                //     println!("{}", echo[1]);
+                // }
+                // else {
+                    // println!("{}: command not found", command)
+                // }
+            },
+        }
+    }
+}
