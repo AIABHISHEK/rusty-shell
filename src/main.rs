111b30f0db5231a07cf111b827a23c058974ec14
ABHISHEK <ay22oct02@gmail.com>
2025-06-06 23:34:21 +0530

    remove unecesary print form main

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod shell;
 mod builtins;
 
 fn main() {
-    for argument in env::args() {
-        println!("{argument}");
-    }
-    match env::var("PATH") {
-        Ok(v)=> println!("{v}"),
-        Err(e)=>println!("error"),
-    }
+    // for argument in env::args() {
+    //     println!("{argument}");
+    // }
+    // match env::var("PATH") {
+    //     Ok(v)=> println!("{v}"),
+    //     Err(e)=>println!("error"),
+    // }
     shell::run();
     
 }
