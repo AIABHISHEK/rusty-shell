21552b4fe8c2341465dd62e2225e9841aa7fe52d
ABHISHEK <ay22oct02@gmail.com>
2025-06-07 23:04:01 +0530

    fix

--- src/shell.rs
+++ src/shell.rs
@@ -34,6 +34,7 @@ pub fn run() {
             //     return;
             // }
             _ => {
+                println!("this is unknown");
                 builtins::existing_command(commandInput);
             }
         }
