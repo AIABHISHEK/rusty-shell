232f7caa05aa40e610276b2e23d0f37ad1d936da
ABHISHEK <ay22oct02@gmail.com>
2025-06-09 15:26:09 +0530

    fix

--- src/builtins.rs
+++ src/builtins.rs
@@ -83,9 +83,9 @@ pub fn existing_command(commandInput: Vec<&str>) {
     match command {
         Some(text) => {
             if let Ok(path_var) = env::var("PATH") {
-                for dir in path_var.split(';') {
+                for dir in path_var.split(':') {
                     let full_path = path::Path::new(dir).join(text);
-                    if full_path.exists() && full_path.is_file() {
+                    if full_path.exists() {
                         // execute command
                         let mut output = process::Command::new(text)
                             // .output()
